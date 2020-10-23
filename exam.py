dd297af1b8a0d5cd4affb0ee63b5b0204909e7b2
Abhiram-23 <53373635+Abhiram-23@users.noreply.github.com>
2020-10-23 11:40:12 +0530

    Add files via upload

---
+++ exam.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Job:
+    def __init__(self,job_id,company,job_role,start_salary,high_salary):
+        self.job_id=job_id
+        self.company=company
+        self.job_role=job_role
+        self.start_salary=start_salary
+        self.high_salary=high_salary
+class Jobsportal:
+    def __init__(self,portal_name,job_list):
+        self.portal_name=portal_name
+        self.job_list=job_list
+    def get_bestjobbyrole(self):
+        pass
+    def jobswithsalary(self,salary):
+        pass
+if __name__=='__main__':
+    a = int(input())
+    for i in range(a):
+        job=int(input())
+        company=input()
+        job_role=input()
+        start_salary=int(input())
+        high_salary=int(input())
+
+
+# In[ ]:
+
+
+a = int(input())
+    for i in range(a):
+        
+        
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
